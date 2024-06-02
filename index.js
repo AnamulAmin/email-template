b9c2884c35c01acb2a5c6231003f87e49f647c60
AnamulAmin <161101326+AnamulAmin@users.noreply.github.com>
2024-06-02 20:12:04 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const cartItems = [
     type: "Silver",
     price: 15,
   },
+  {
+    seatName: "E15",
+    type: "Silver",
+    price: 15,
+  },
+  {
+    seatName: "E15",
+    type: "Silver",
+    price: 15,
+  },
 ];
 
 const customerInfo = {
@@ -16,15 +26,14 @@ const discount = 0;
 
 const customerAddress = {
   heading: "Deyaler Desh at Dumaresq Campbelltown on 12th May Sunday 6 PM",
-  address: `12 May, 2024 at 06:00 PM
-    Dumaresq Street Cinema, NSW - Dumaresq Cinema 3
-    4 Dumaresq St, Campbelltown, NSW 2560
-    Coupon Used : N/A`,
+  address: '12 May, 2024 at 06:00 PM Dumaresq Street Cinema, NSW - Dumaresq Cinema 3 4 Dumaresq St, Campbelltown, NSW 2560 Coupon Used : N/A',
 };
 
 const totalPrice = cartItems?.reduce((sum, item) => {
-  return sum + item.price * item.quantity;
+  return sum + item.price;
 }, 0);
+
+console.log(totalPrice)
 const totalPriceWithCharges = Math.round(totalPrice - discount);
 
 // orderDate = new Date(orderDate).toLocaleString();
@@ -34,6 +43,65 @@ const message = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
 
   <head>
     <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+<style>
+  *{
+    font-family: "Poppins", sans-serif;
+  }
+  .logo{
+    display:block;
+    border:0;
+    outline:none;
+    text-decoration:none;
+    -ms-interpolation-mode:bicubic; 
+    width: 150px;
+  }
+  .invoice_to{
+    Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:15px;color:#333333;font-size:14px; font-weight: bold; padding-right: 10px; padding-bottom: 7px;
+  }
+
+  .footer_p{
+    font-size: 1.2rem;
+    font-weight: 700;
+    line-height: 1;
+  }
+  .border_bottom{
+    position: absolute;
+    top: -10px;
+    left: 0;
+    width: 100%;
+  }
+  .customerName{
+    font-size:16px;line-height:1.4;margin:0;padding:0;color:rgb(102,102,102) padding-top: 60px;
+  }
+  .customerName{
+    font-size:16px;line-height:1.4;margin:0;padding:0color:rgb(102,102,102); width: 200px;
+  }
+  .invoiceTitle{
+    padding: 1rem 3rem; border: none; background-color: #fff; font-size: 1.7rem; font-weight: 700; margin-top: 3rem; padding-left: 0;
+  }
+  thead{
+    border-collapse:collapse;border-spacing:1rem;color:rgb(51,51,51);background-color:rgb(250,250,250);font-size:12px; padding: 0 .5rem;
+  }
+  .payment_info{
+    font-size:16px;line-height:1rem;margin:0;color:rgb(102,102,102);font-weight:600;
+  }
+  .th_name{
+    font-size:14px;line-height:24px;margin:0;font-weight:900
+  }
+  .order_id{
+    font-size:14px; 
+    font-weight: 500;
+    line-height:1.4;
+    margin:0;
+    padding:0color:rgb(102,102,102);
+     width: 200px;
+    color:rgb(102,102,102);
+  }
+</style>
   </head>
   </div>
 
@@ -49,7 +117,7 @@ const message = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
                     <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation">
                       <tbody style="width:100%">
                         <tr style="width:100%">
-                          <td data-id="__react-email-column"><img class="adapt-img" src="https://www.bongozfilms.com/frontend/images/logo.png" alt style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic" width="186">
+                          <td data-id="__react-email-column"><img src="https://www.bongozfilms.com/frontend/images/logo.png" alt="logo width="100" class="logo" />
                         </tr>
                       </tbody>
                     </table>
@@ -58,7 +126,7 @@ const message = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
               </tbody>
             </table>
             <table align="center" width="100%" border="0" cellPadding="10" cellSpacing="" role="presentation">
-              <tbody>
+              <tbody style'padding'>
                 <tr>
                     <td valign="top" align="center" style="padding:0;Margin:0;width:560px">
                         <table width="100%" cellspacing="0" cellpadding="0" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
@@ -66,129 +134,114 @@ const message = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
                           <tr>
                            </td>
                           <td align="left" data-id="__react-email-column" style="display:table-cell ">
-                            <button style="padding: 1rem 3rem; border: none; background-color: #fff; font-size: 1.7rem; font-weight: 700; margin-top: 3rem; padding-left: 0" >Invoice</button>
+                            <button style="" class="invoiceTitle" >Invoice</button>
 
-                            <p style="font-size:14px; font-weight: 500;line-height:1.4;margin:0;padding:0color:rgb(102,102,102); width: 200px; color:rgb(102,102,102);">Order ID      10 May, 2024${
+                            <p class="order_id">Order ID 10 May, 2024${
                               customerInfo?.orderId
                             }</p>
                           </td>
                           <td align="right" colSpan="2" data-id="__react-email-column" style="border-style:solid;border-color:white;border-width:0px 1px 1px 0px;height:44px">
-                          <p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:15px;color:#333333;font-size:14px; font-weight: bold; padding-right: 10px; padding-bottom: 7px" >Invoice To:</p>
-                          <p style="font-size:16px;line-height:1.4;margin:0;padding:0;color:rgb(102,102,102) padding-top: 60px;">${
+                          <p style="invoice_to" >Invoice To:</p>
+                          <p class="customerName" >${
                             customerInfo?.name
                           }</p>
-                          <p style="font-size:16px;line-height:1.4;margin:0;padding:0color:rgb(102,102,102); width: 200px;">${
+                          <p className="customer_address" >${
                             customerInfo?.address
                           }</p>
                         </td>
-                </tr>
-                          <tr >
-                           <td align="right" style="padding:0;Margin:0">
-                             
-                        </td>
-                </tr>
                 <tr>
                 <tr style="width:100%">
-                                        <td align="" colspan='2' style="width: 100%" data-id="__react-email-column" style="border-style:solid;border-color:white;border-width:0px 1px 1px 0px;height:44px">
-                                          <p style="font-size:16px;line-height:1.4;margin:0;padding:.2rem 0; font-weight: 700; padding-top: 1.5rem;">${
-                                            customerAddress.heading
-                                          }</p>
-                                          <p style="font-size:13px;line-height:1.4;margin:0;padding:.2rem 0;color:rgb(102,102,102); font-weight: 400; max-width: 300px; padding-top: 0.5rem;">${
-                                            customerAddress.address
-                                          }</p>
-                                        </td>
+                    <td align="" colSpan='2' style="width: 100%" data-id="__react-email-column" style="border-style:solid;border-color:white;border-width:0px 1px 1px 0px;height:44px">
+                      <p style="font-size:16px;line-height:1.4;margin:0;padding:.2rem 0; font-weight: 700; padding-top: 1.5rem;">${customerAddress.heading}</p>
+                    <p style="font-size:13px;line-height:1.4;margin:0;padding:.2rem 0;color:rgb(102,102,102); font-weight: 400; max-width: 300px; padding-top: 0.5rem; padding-bottom: 1rem">${customerAddress.address}</p>
+                  </td>
                 </tr>
               </tr>
               </tbody>
             </table>
             
-            <p style=" font-size: 5px; opacity: 0; visibility: hidden;">space</p>
            
-            <table align="right" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation">
-              <tbody>
-                <tr>
-                  <td>
-                    <table align="center" width="100%" border="0" cellPadding="10" cellSpacing="0" role="presentation">
-                        <thead style="border-collapse:collapse;border-spacing:1rem;color:rgb(51,51,51);background-color:rgb(250,250,250);font-size:12px; padding: 0 .5rem;">
+            <table align="center" width="100%" border="0" cellPadding="10" cellSpacing="0" role="presentation">
+                        <thead style="" >
                             <tr style="background-color: #F5D930;">
                                 <th align="left">
-                                  <p style="font-size:14px;line-height:24px;margin:0;font-weight:900">Name</p>
+                                  <p class='th_name'>Seat No.</p>
                                 </th>
-                                <th >
-                                  <p style="font-size:14px;line-height:24px;margin:0;font-weight:900">Color</p>
+                                <th  >
+                                  <p class='th_name' >Type</p>
                                 </th>
-                                <th >
-                                  <p style="font-size:14px;line-height:24px;margin:0;font-weight:900">Size</p>
-                                </th>
-                                <th >
-                                  <p style="font-size:14px;line-height:24px;margin:0;font-weight:900">Quantity</p>
-                                </th>
-                                <th >
-                                  <p style="font-size:14px;line-height:24px;margin:0;font-weight:900">Unit price</p>
-                                </th>
-                                <th >
-                                  <p style="font-size:14px;line-height:24px;margin:0;font-weight:900">Total Price</p>
+                                <th>
+                                  <p style="text-align: right" class='th_name'>Price</p>
                                 </th>
                               </tr>
                         </thead>
                       <tbody style="width:100%">
                       ${cartItems?.map(
                         (paymentInfo) =>
-                          `<tr style="width:100%; border-bottom: 1px solid black">
+                          `
+                        <tr style="width:100%; border-bottom: 1px solid black">
                             
                           <td align="left" data-id="__react-email-column" style="display:table-cell; ">
-                            <p style="font-size:10px;line-height:1rem;margin:0;color:rgb(102,102,102);font-weight:600; max-width: 100px;">${paymentInfo.seatName}</p>
+                            <p class="payment_info">${paymentInfo.seatName}</p>
                           </td>
                           
                           <td align="left" data-id="__react-email-column" style="display:table-cell; ">
-                            <p style="font-size:10px;line-height:24px;margin:0;color:rgb(102,102,102);font-weight:600; text-align: center;">${paymentInfo?.type}</p>
+                            <p style="text-align: center;" class="payment_info">${paymentInfo?.type}</p>
                           </td>
                           <td align="left" data-id="__react-email-column" style="display:table-cell; ">
-                            <p style="font-size:16px;line-height:24px;margin:0;color:rgb(102,102,102);font-weight:600; text-align: center;">${paymentInfo.price}</p>
+                            <p style="text-align: right;" class="payment_info">${paymentInfo.price}</p>
                           </td>
-                          <td align="right" data-id="__react-email-column" style="display:table-cell;width:90px">
-                            <p style="font-size:16px;line-height:24px;margin:0px 20px 0px 0px;font-weight:600;white-space:nowrap; text-align: center;">$${totalPriceWithCharges}</p>
+                        </tr>
+                        <tr style="width:100%; border-bottom: 1px solid black">
+                          <td colSpan="3" data-id="__react-email-column" style="position: relative;">
+                            <p style="border-bottom: 1px solid #999999" class="border_bottom"></p>
                           </td>
-                        </tr>`
-                      )}
+                        </tr>
+                        `
+                      ).join('')}
 
                         
                         
                       </tbody>
                       
                     </table>
-                  </td>
-                  
-                </tr>
-              </tbody>
-            </table>
       
             
             <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation">
               <tbody>
                 <tr>
                   <td>
-                    <p><span>Subtotal</span> $${totalPriceWithCharges}</p>
-                    <p><span>Discount</span> $${discount}</p>
-                    <p><span>Discount</span> $${discount}</p>
+                  <div style='width: 50%; margin-left: auto; padding-right: 8px'>
+                  <p style="display: flex; justify-content: space-between; gap: 10px"><span>Subtotal</span> $${totalPrice}</p>
+                  <p style="display: flex; justify-content: space-between; gap: 10px"><span>Discount</span> $${discount}</p>
+                  <p style="display: flex; justify-content: space-between; gap: 10px"><span style="font-weight: bold;" >Grand Total</span> $${totalPriceWithCharges}</p>
+                  </div>
                   </td>
+                </tr>
+                <tr>
+                <td>
+                <p style="padding-bottom: 50px; border-bottom: 2px solid #999999;">Please login to your BongOz Films account to find your booked seats. Just show this at the venue for entry.</p>
+                </td>
+                </tr>
+                <tr>
+                  <td>
+                  <div style='width: 50%;'>
+                  <p class="footer_p"> THANK YOU!</p>
+                  <p class="footer_p"> BongOz Films Team</p>
+                  <p class="footer_p"> Email: <a href="mailto:info@bongozfilms.com">info@bongozfilms.com</a></p>
+                  <p class="footer_p"> Mobile: +61 406 063 058</p>
+                  </div>
+                  </td>
+                  
                 </tr>
               </tbody>
             </table>
-            <p style="font-size:12px;line-height:24px;margin:8px 0 0 0;text-align:center;color:rgb(102,102,102)">
-            <p style="font-size:12px;line-height:24px;margin:25px 0 0 0;text-align:center;color:rgb(102,102,102)">Copyright © 2023 CheB Inc. <br /> All rights reserved</p>
-      
-      
-      
-            
           </td>
         </tr>
       
         
       </tbody>
       </table>
-
-    <script src="./main.js"></script>
   </body>
 
 </html>`;
